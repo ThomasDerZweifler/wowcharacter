6aad116a388eec0c160f19607a1b271ac423a967
thomasfunke71 <thomasfunke71@3038667e-face-11dd-ada4-5153b8187bf2>
2009-02-15 15:48:53 +0000

    finish()

--- WoW Character/src/de/stm/android/wow/character/Splash.java
+++ WoW Character/src/de/stm/android/wow/character/Splash.java
@@ -1,5 +1,6 @@
 package de.stm.android.wow.character;
 
+import android.app.Activity;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
@@ -8,33 +9,31 @@ import android.view.View.OnClickListener;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class Splash implements Constants {
-	private Wowcharacter parent;
-	
-	public Splash (Wowcharacter parent) {
-		this.parent = parent;
-		init();
-	}
+public class Splash extends Activity implements OnClickListener, Constants {
 	
 	private void init() {
-		parent.setContentView(R.layout.splash);				
-        ((RelativeLayout) parent.findViewById(R.id.splash)).setOnClickListener(mSplashClickListener);
+		setContentView(R.layout.splash);				
+        ((RelativeLayout) findViewById(R.id.splash)).setOnClickListener(this);
         
-        TextView mTextView = (TextView) parent.findViewById(R.id.splash_label_ver);
+        TextView mTextView = (TextView) findViewById(R.id.splash_label_ver);
 
         try {
-            PackageInfo pi = parent.getPackageManager().getPackageInfo(parent.getPackageName(), 0);
+            PackageInfo pi = getPackageManager().getPackageInfo(getPackageName(), 0);
             mTextView.setText("Version: " + pi.versionName);
         } catch (NameNotFoundException e) {
             // should never happen 
         }; 
 	}
 	
-    OnClickListener mSplashClickListener = new OnClickListener() {
-        public void onClick(View v) {
-            Bundle stats = new Bundle();
-            stats.putString("param1","0"); 
-            parent.request(SPLASH_CLICKED, 0, stats);       	
-        }
-    };
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        init();
+    }
+	
+    public void onClick(View v) {
+        setResult(RESULT_OK);
+        finish();
+    }
 }
