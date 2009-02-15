cb3ab749d3a132ad729457e607c4e0a43a3d38db
thomasfunke71 <thomasfunke71@3038667e-face-11dd-ada4-5153b8187bf2>
2009-02-15 15:48:53 +0000

    finish()

--- trunk/WoW Character/src/de/stm/android/wow/character/Wowcharacter.java
+++ trunk/WoW Character/src/de/stm/android/wow/character/Wowcharacter.java
@@ -1,29 +1,27 @@
 package de.stm.android.wow.character;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Wowcharacter extends Activity implements Constants {
 
-	// Listen for results.
-	protected void request(int requestCode, int resultCode, Bundle data){
-	    // See which child activity is calling us back.
-	    switch (requestCode) {
-	        case SPLASH_CLICKED:
-	        	System.out.println("splash clicked.");
-	        	data.getString("param1");
+    protected void onActivityResult(int requestCode, int resultCode,
+            Intent data) {
+    	
+    	if (requestCode == SPLASH_CLICKED) {
+            if (resultCode == RESULT_OK) {
 	    		setContentView(R.layout.search);
-
-	        default:
-	            break;
-	    }
-	}
+            }
+        }
+    }
 
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        new Splash(this);
+        Intent intent = new Intent(this, de.stm.android.wow.character.Splash.class);
+        startActivityForResult(intent, SPLASH_CLICKED);
     }
     
 }
