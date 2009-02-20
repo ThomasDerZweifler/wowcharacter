92635fa43ffd38affee1dc87513b418b03eb81b6
thomasfunke71 <thomasfunke71@3038667e-face-11dd-ada4-5153b8187bf2>
2009-02-20 14:30:02 +0000

    deflate gunzip

--- trunk/WoW Character/src/de/stm/android/wow/character/Search.java
+++ trunk/WoW Character/src/de/stm/android/wow/character/Search.java
@@ -1,13 +1,23 @@
 package de.stm.android.wow.character;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.zip.GZIPInputStream;
+import java.util.zip.Inflater;
+import java.util.zip.InflaterInputStream;
 
-import javax.xml.parsers.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
-import org.w3c.dom.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
@@ -15,7 +25,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 /**
  * Suchdialog
@@ -107,7 +120,6 @@ public class Search extends Activity {
 	 * @return
 	 */
 	public StringBuilder getXML( String strURL, boolean packed ) {
-		StringBuilder sb = null;
 		if (!strURL.startsWith( "http://" )) {
 			strURL = "http://" + strURL;
 			et.setText( strURL );
@@ -125,46 +137,50 @@ public class Search extends Activity {
 //							"Mozilla/5.0 (Windows; U; Windows NT 6.0; rv:1.9.1b3pre) Gecko/20090218 Firefox/3.0 SeaMonkey/2.0a3pre" );
 //			urlConn.addRequestProperty( "Accept",
 //					"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" );
-//			urlConn.addRequestProperty( "Accept", "de-de,de;q=0.8,en-us;q=0.5,en;q=0.3" );
-			urlConn.addRequestProperty( "Accept-Encoding", packed ? "gzip,deflate" : "identity" );
 //			urlConn.addRequestProperty( "Accept-Charset", "ISO-8859-15,utf-8;q=0.7,*;q=0.7" );
+//			urlConn.addRequestProperty( "Accept-Charset", "ISO-8859-1,ISO-8859-2" );
+
+//			urlConn.addRequestProperty( "Accept-Language", "de-de,de;q=0.8" );
+			urlConn.addRequestProperty( "Accept-Language", "en-us;q=0.5,en;q=0.3" );
+
+			urlConn.addRequestProperty( "Accept-Encoding", packed ? "gzip,deflate" : "identity" );
 		} catch (IOException ioe) {
 			Log.e( "Search", "Could not connect to server!" );
 			tv.setText( "check url!" );
 			return null;
 		}
 		InputStream is = null;
-		BufferedReader br = null;
+		InputStreamReader isr = null;
+		StringBuilder sb = null;
 		try {
 			is = urlConn.getInputStream();
-			if (packed) {
-				is = new GZIPInputStream( is );
+			String encoding = urlConn.getContentEncoding();
+			if (encoding != null ) {
+				if( encoding.equalsIgnoreCase("gzip") ) {
+					is = new GZIPInputStream( is );					
+				} else if( encoding.equalsIgnoreCase("deflate") ) {
+					is = new InflaterInputStream( is, new Inflater( true ) );
+				}
 			}
-			InputStreamReader isr = new InputStreamReader( is );
-			br = new BufferedReader( isr );
+			isr = new InputStreamReader( is );
 			sb = new StringBuilder();
 			char chars[] = new char[1024];
 			int len = 0;
-			while ((len = br.read( chars, 0, chars.length )) >= 0) {
+			while ((len = isr.read( chars, 0, chars.length )) >= 0) {
 				sb.append( chars, 0, len );
 			}
-			// -----------funktioniert nicht!?-----------
-			// String line = null;
-			// while ((line = br.readLine()) != null) {
-			// sb.append( line );
-			// }
-			// ------------------------------------------
-			tv.setText( sb );
-			sv.scrollTo( 0, 0 );
+			if( sb.length() > 0 ) {
+				tv.setText( sb );				
+				sv.scrollTo( 0, 0 );
+			}
 		} catch (IOException ioe) {
 			Log.e( "Search", "Invalid format!!" );
 			tv.setText( "check page!" );
 		} finally {
 			try {
-				is.close();
-				br.close();
-			} catch (IOException ioExc) {
-				Log.e( "Search", "Exception Occured in finally block" + ioExc.getMessage() );
+				isr.close();
+			} catch (Exception e) {
+				Log.e( "Search", "finally" + e.getMessage() );
 			}
 		}
 		return sb;
