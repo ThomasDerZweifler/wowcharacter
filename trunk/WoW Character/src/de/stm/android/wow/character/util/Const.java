bd1658a59565ca4a0178334a1f9da3dc5ab8849e
thomasfunke71 <thomasfunke71@3038667e-face-11dd-ada4-5153b8187bf2>
2009-02-21 19:10:55 +0000

    EU/US

--- trunk/WoW Character/src/de/stm/android/wow/character/util/Const.java
+++ trunk/WoW Character/src/de/stm/android/wow/character/util/Const.java
@@ -1,6 +1,11 @@
 package de.stm.android.wow.character.util;
 
 public interface Const {
+	static enum Region {
+		EU,
+		US
+	};
+	
 	final static public String URL_EU = "http://eu.wowarmory.com/";
 	final static public String URL_US = "http://www.wowarmory.com/";
 	
