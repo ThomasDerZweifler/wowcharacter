b23d5fc625626eaa9598745d676cdb14d57367a0
thomasfunke71 <thomasfunke71@3038667e-face-11dd-ada4-5153b8187bf2>
2009-02-21 21:00:20 +0000

    streams schliessen

--- trunk/WoW Character/src/de/stm/android/wow/character/util/Armory.java
+++ trunk/WoW Character/src/de/stm/android/wow/character/util/Armory.java
@@ -19,32 +19,41 @@ public class Armory implements Const {
 	 * @param packed
 	 * @return
 	 */
-	private StringBuilder getXML( String strURL, boolean packed ) {
-		if (!strURL.startsWith( "http://" )) {
+	private StringBuilder getXML(String strURL, boolean packed) {
+		if (!strURL.startsWith("http://")) {
 			strURL = "http://" + strURL;
 		}
 		URLConnection urlConn = null;
 		try {
-			URL url = new URL( strURL );
+			URL url = new URL(strURL);
 			// Wichtig, Zugriff erlauben durch: <uses-permission
 			// android:name="android.permission.INTERNET" /> (in manifest)
 			urlConn = url.openConnection();
-			urlConn.addRequestProperty( "User-Agent", "Firefox/3.0" );
-//			urlConn
-//					.addRequestProperty(
-//							"User-Agent",
-//							"Mozilla/5.0 (Windows; U; Windows NT 6.0; rv:1.9.1b3pre) Gecko/20090218 Firefox/3.0 SeaMonkey/2.0a3pre" );
-//			urlConn.addRequestProperty( "Accept",
-//					"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" );
-//			urlConn.addRequestProperty( "Accept-Charset", "ISO-8859-15,utf-8;q=0.7,*;q=0.7" );
-//			urlConn.addRequestProperty( "Accept-Charset", "ISO-8859-1,ISO-8859-2" );
+			urlConn.addRequestProperty("User-Agent", "Firefox/3.0");
+			urlConn.setRequestProperty("Connection", "close");
+			urlConn.setConnectTimeout(1000 * 10 * 60); // timeout after 10
+														// minutes
+			// urlConn
+			// .addRequestProperty(
+			// "User-Agent",
+			// "Mozilla/5.0 (Windows; U; Windows NT 6.0; rv:1.9.1b3pre)
+			// Gecko/20090218 Firefox/3.0 SeaMonkey/2.0a3pre" );
+			// urlConn.addRequestProperty( "Accept",
+			// "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+			// );
+			// urlConn.addRequestProperty( "Accept-Charset",
+			// "ISO-8859-15,utf-8;q=0.7,*;q=0.7" );
+			// urlConn.addRequestProperty( "Accept-Charset",
+			// "ISO-8859-1,ISO-8859-2" );
 
-			urlConn.addRequestProperty( "Accept-Language", "de-de,de;q=0.8" );
-//			urlConn.addRequestProperty( "Accept-Language", "en-us;q=0.5,en;q=0.3" );
+			urlConn.addRequestProperty("Accept-Language", "de-de,de;q=0.8");
+			// urlConn.addRequestProperty( "Accept-Language",
+			// "en-us;q=0.5,en;q=0.3" );
 
-			urlConn.addRequestProperty( "Accept-Encoding", packed ? "gzip,deflate" : "identity" );
+			urlConn.addRequestProperty("Accept-Encoding",
+					packed ? "gzip,deflate" : "identity");
 		} catch (IOException ioe) {
-			Log.e( "Armory", "Could not connect to server!" );
+			Log.e("Armory", "Could not connect to server!");
 			return null;
 		}
 		InputStream is = null;
@@ -53,27 +62,26 @@ public class Armory implements Const {
 		try {
 			is = urlConn.getInputStream();
 			String encoding = urlConn.getContentEncoding();
-			if (encoding != null ) {
-				if( encoding.equalsIgnoreCase("gzip") ) {
-					is = new GZIPInputStream( is );					
-				} else if( encoding.equalsIgnoreCase("deflate") ) {
-					is = new InflaterInputStream( is, new Inflater( true ) );
+			if (encoding != null) {
+				if (encoding.equalsIgnoreCase("gzip")) {
+					is = new GZIPInputStream(is);
+				} else if (encoding.equalsIgnoreCase("deflate")) {
+					is = new InflaterInputStream(is, new Inflater(true));
 				}
 			}
-			isr = new InputStreamReader( is );
+			isr = new InputStreamReader(is);
 			sb = new StringBuilder();
 			char chars[] = new char[1024];
 			int len = 0;
-			while ((len = isr.read( chars, 0, chars.length )) >= 0) {
-				sb.append( chars, 0, len );
+			while ((len = isr.read(chars, 0, chars.length)) >= 0) {
+				sb.append(chars, 0, len);
 			}
+			isr.close();
 			is.close();
-		}
-		catch( Exception e ) {
+		} catch (Exception e) {
 			/** */
-		}
-		catch (Throwable t) {
-			Log.e( "Armory", "Exception: "+ t.toString() );
+		} catch (Throwable t) {
+			Log.e("Armory", "Exception: " + t.toString());
 		}
 		return sb;
 	}
@@ -86,13 +94,12 @@ public class Armory implements Const {
 	 */
 	public StringBuilder search(String character, int region) {
 		String r = URL_EU;
-		if( region == Region.US.ordinal() ) {
+		if (region == Region.US.ordinal()) {
 			r = URL_US;
 		}
-		
+
 		String url = r + SEARCHPAGE + character + SEARCHTYPE_ALL;
 		return getXML(url, true);
 	}
 
-	
 }
