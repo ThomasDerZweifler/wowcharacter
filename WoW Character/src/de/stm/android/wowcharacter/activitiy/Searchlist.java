66492f86d01cc5ea1672b8958c44c29a6c711ece
thomasfunke71 <thomasfunke71@3038667e-face-11dd-ada4-5153b8187bf2>
2009-05-13 19:32:57 +0000

    doppelte suche gefixt

--- WoW Character/src/de/stm/android/wowcharacter/activitiy/Searchlist.java
+++ WoW Character/src/de/stm/android/wowcharacter/activitiy/Searchlist.java
@@ -36,8 +36,8 @@ import de.stm.android.wowcharacter.xml.InterpretSearch;
 /**
  * Suchdialog
  * 
- * @author <a href="mailto:thomasfunke71@googlemail.com">Thomas Funke</a>,
- * <a href="mailto:stefan.moldenhauer@googlemail.com">Stefan Moldenhauer</a>
+ * @author <a href="mailto:thomasfunke71@googlemail.com">Thomas Funke</a>, <a
+ *         href="mailto:stefan.moldenhauer@googlemail.com">Stefan Moldenhauer</a>
  * 
  */
 public class Searchlist extends ListActivity {
@@ -55,40 +55,41 @@ public class Searchlist extends ListActivity {
 	ArrayList<Character> listModel = new ArrayList<Character>();
 	Handler handler = new Handler() {
 		@Override
-		public void handleMessage( Message msg ) {
-            setProgressBarIndeterminateVisibility(false);
-			bt.setEnabled( true );
+		public void handleMessage(Message msg) {
+			setProgressBarIndeterminateVisibility(false);
+			bt.setEnabled(true);
 			if (sbXMLPage != null && sbXMLPage.length() > 0) {
 				Region region = Region.EU;
 				if (tb_US.isChecked()) {
 					region = Region.US;
 				}
 				listModel.clear();
-				is.readXML( sbXMLPage.toString(), region, listModel );
-				SearchListAdapter sla = new SearchListAdapter( Searchlist.this, listModel );
-				Collections.sort( listModel );
+				is.readXML(sbXMLPage.toString(), region, listModel);
+				SearchListAdapter sla = new SearchListAdapter(Searchlist.this,
+						listModel);
+				Collections.sort(listModel);
 
-				setListAdapter( sla );					
+				setListAdapter(sla);
 
 				String s;
 				int listsize = listModel.size();
 				if (listsize == 0) {
-					s = getString( R.string.search_char_found_none_toast );
+					s = getString(R.string.search_char_found_none_toast);
 				} else if (listsize == 1) {
-					s = getString( R.string.search_char_found_one_toast );
+					s = getString(R.string.search_char_found_one_toast);
 				} else {
-					s = getString( R.string.search_char_found_more_toast );
-					s = s.replace( "%1", Integer.toString( listsize ) );					
+					s = getString(R.string.search_char_found_more_toast);
+					s = s.replace("%1", Integer.toString(listsize));
 				}
-				Toast.makeText( Searchlist.this, s, Toast.LENGTH_SHORT ).show();
-				
+				Toast.makeText(Searchlist.this, s, Toast.LENGTH_SHORT).show();
+
 			}
 		}
 	};
 
 	@Override
-	public void onCreate( Bundle savedInstanceState ) {
-		super.onCreate( savedInstanceState );
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		init();
 	}
 
@@ -97,128 +98,137 @@ public class Searchlist extends ListActivity {
 	 */
 	private void init() {
 		model = Model.getInstance();
-        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);//fuer Fortschrittskreis in Titelzeile
+		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);// fuer
+																	// Fortschrittskreis
+																	// in
+																	// Titelzeile
 		/** View und Titel setzen */
-		setContentView( R.layout.searchlist );
-        setProgressBarIndeterminateVisibility(false);
-		String sAppName = getString( R.string.app_name );
-		String sTitle = getString( R.string.search_title );
-		setTitle( sAppName + " (" + sTitle + ")" );
+		setContentView(R.layout.searchlist);
+		setProgressBarIndeterminateVisibility(false);
+		String sAppName = getString(R.string.app_name);
+		String sTitle = getString(R.string.search_title);
+		setTitle(sAppName + " (" + sTitle + ")");
 		/** Textfeld finden */
-		et = (EditText)findViewById( R.id.editTextName );
-		et.addTextChangedListener( new TextWatcher() {
-			public void afterTextChanged( Editable arg0 ) {
+		et = (EditText) findViewById(R.id.editTextName);
+		et.addTextChangedListener(new TextWatcher() {
+			public void afterTextChanged(Editable arg0) {
 				textChanged();
 			}
 
-			public void beforeTextChanged( CharSequence arg0, int arg1, int arg2, int arg3 ) {
+			public void beforeTextChanged(CharSequence arg0, int arg1,
+					int arg2, int arg3) {
 				textChanged();
 			}
 
-			public void onTextChanged( CharSequence arg0, int arg1, int arg2, int arg3 ) {
+			public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {
 				textChanged();
 			}
 
 			private void textChanged() {
 				handleTextChanged();
 			}
-		} );
-		et.setOnKeyListener( new EditText.OnKeyListener() {
-			public boolean onKey( View v, int keyCode, KeyEvent event ) {
+		});
+		et.setOnKeyListener(new EditText.OnKeyListener() {
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
 				if (keyCode == KeyEvent.KEYCODE_ENTER) {
-					bt.performClick();
+					bt.dispatchKeyEvent(event);
 					return true;
 				}
 				return false;
 			}
-		} );
+		});
 		/** OnClickListener für Suchenbutton setzen */
-		bt = (ImageButton)findViewById( R.id.buttonSearch );
-		bt.setOnClickListener( new Button.OnClickListener() {
-			public void onClick( View v ) {
-				bt.setEnabled( false );
-                setProgressBarIndeterminateVisibility(true);
-				searchThread = new Thread( new Runnable() {
+		bt = (ImageButton) findViewById(R.id.buttonSearch);
+		bt.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				bt.setEnabled(false);
+				setProgressBarIndeterminateVisibility(true);
+				searchThread = new Thread(new Runnable() {
 					public void run() {
 						try {
-							sbXMLPage = armory.search( et.getText().toString(), region );
-							handler.sendMessage( handler.obtainMessage() );
+							sbXMLPage = armory.search(et.getText().toString(),
+									region);
+							handler.sendMessage(handler.obtainMessage());
 						} catch (Throwable t) {
 							// just end the background thread
 						}
 					}
-				}, "WOW-Search" );
+				}, "WOW-Search");
 				searchThread.start();
 			}
-		} );
-		bt.setEnabled( false );
+		});
+		bt.setEnabled(false);
 		/** Togglebuttonfunktionalität */
-		tb_EU = (ToggleButton)findViewById( R.id.toggle_EU );
-		tb_US = (ToggleButton)findViewById( R.id.toggle_US );
-		tb_EU.setOnClickListener( new OnClickListener() {
-			public void onClick( View arg0 ) {
+		tb_EU = (ToggleButton) findViewById(R.id.toggle_EU);
+		tb_US = (ToggleButton) findViewById(R.id.toggle_US);
+		tb_EU.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
 				region = Region.EU;
-				tb_US.setChecked( false );// Radio-Button Funktionalitaet
+				tb_US.setChecked(false);// Radio-Button Funktionalitaet
 				if (!tb_US.isChecked()) {
-					tb_EU.setChecked( true );
+					tb_EU.setChecked(true);
 				}
 			}
-		} );
-		tb_US.setOnClickListener( new OnClickListener() {
-			public void onClick( View arg0 ) {
+		});
+		tb_US.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
 				region = Region.US;
-				tb_EU.setChecked( false );// Radio-Button Funktionalitaet
+				tb_EU.setChecked(false);// Radio-Button Funktionalitaet
 				if (!tb_EU.isChecked()) {
-					tb_US.setChecked( true );
+					tb_US.setChecked(true);
 				}
 			}
-		} );
+		});
 		/**
-		 * - bei Initialisierung gem. der Ländereinstellung bereits einen Togglebutton markieren
-		 * (bei der Auswahl der Sparche wird in Android bei deutsch nur de geliefert (ohne
-		 * Ländercode) daher Fallback von getCountry() auf getLanguage() eingebaut
+		 * - bei Initialisierung gem. der Ländereinstellung bereits einen
+		 * Togglebutton markieren (bei der Auswahl der Sparche wird in Android
+		 * bei deutsch nur de geliefert (ohne Ländercode) daher Fallback von
+		 * getCountry() auf getLanguage() eingebaut
 		 */
 		String locale = Locale.getDefault().getCountry();
 		if (locale.length() == 0) {
 			locale = Locale.getDefault().getLanguage();
 		}
-		if (locale.equalsIgnoreCase( "US" )) {
-			tb_US.setChecked( true );
+		if (locale.equalsIgnoreCase("US")) {
+			tb_US.setChecked(true);
 			// tb_EU.setChecked(false);
 		} else {
-			tb_EU.setChecked( true );
+			tb_EU.setChecked(true);
 			// tb_US.setChecked(false);
 		}
 		/** Kontextmenü registrieren */
-		registerForContextMenu( getListView() );
+		registerForContextMenu(getListView());
 	}
 
 	@Override
-	public void onCreateContextMenu( ContextMenu menu, View v, ContextMenuInfo menuInfo ) {
-		new MenuInflater( getApplication() ).inflate( R.menu.searchcontext, menu );
-		Character character = (Character)getListAdapter().getItem(
-				((AdapterView.AdapterContextMenuInfo)menuInfo).position );
-		menu.setHeaderTitle( character.toString() );
-		super.onCreateContextMenu( menu, v, menuInfo );
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		new MenuInflater(getApplication()).inflate(R.menu.searchcontext, menu);
+		Character character = (Character) getListAdapter().getItem(
+				((AdapterView.AdapterContextMenuInfo) menuInfo).position);
+		menu.setHeaderTitle(character.toString());
+		super.onCreateContextMenu(menu, v, menuInfo);
 	}
 
 	@Override
-	public boolean onContextItemSelected( MenuItem item ) {
+	public boolean onContextItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case R.id.searchcontextmenu_add_favorite:
-			AdapterView.AdapterContextMenuInfo cmi = (AdapterView.AdapterContextMenuInfo)item
+			AdapterView.AdapterContextMenuInfo cmi = (AdapterView.AdapterContextMenuInfo) item
 					.getMenuInfo();
-			Character character = (Character)getListAdapter().getItem( cmi.position );
+			Character character = (Character) getListAdapter().getItem(
+					cmi.position);
 			try {
-				Model.getInstance().addFavorite( character );		
-				//Don 't call it Schnitzel;o)
-				String s = getString( R.string.search_addToFavorites_ok_toast );
-				s = s.replace( "%1", character.toString() );
-				Toast.makeText( this, s, Toast.LENGTH_SHORT ).show();
+				Model.getInstance().addFavorite(character);
+				// Don 't call it Schnitzel;o)
+				String s = getString(R.string.search_addToFavorites_ok_toast);
+				s = s.replace("%1", character.toString());
+				Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
 			} catch (Exception e) {
-				String s = getString( R.string.search_addToFavorites_fail_toast );
-				s = s.replace( "%1", character.toString() );
-				Toast.makeText( this, s, Toast.LENGTH_SHORT ).show();
+				String s = getString(R.string.search_addToFavorites_fail_toast);
+				s = s.replace("%1", character.toString());
+				Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
 			}
 			return true;
 		}
@@ -226,9 +236,9 @@ public class Searchlist extends ListActivity {
 	}
 
 	@Override
-	protected void onListItemClick( ListView l, View v, int position, long id ) {
-		Character character = (Character)getListAdapter().getItem( position );
-		model.getInfos( character );
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		Character character = (Character) getListAdapter().getItem(position);
+		model.getInfos(character);
 	}
 
 	/**
@@ -237,9 +247,9 @@ public class Searchlist extends ListActivity {
 	private void handleTextChanged() {
 		// Suchbutton de-/aktivieren
 		if (et.getText().toString().length() == 0) {
-			bt.setEnabled( false );
+			bt.setEnabled(false);
 		} else {
-			bt.setEnabled( true );
+			bt.setEnabled(true);
 		}
 	}
 }
