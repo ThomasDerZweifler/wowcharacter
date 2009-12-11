796705e4d7019ac952ba79942b51328a31797aa5
thomasfunke71 <thomasfunke71@3038667e-face-11dd-ada4-5153b8187bf2>
2009-12-11 23:22:01 +0000

    items fuellen

---
+++ trunk/WoW Character/src/de/stm/android/wowcharacter/renderer/ItemListAdapter.java
@@ -0,0 +1,50 @@
+package de.stm.android.wowcharacter.renderer;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.view.*;
+import android.widget.*;
+import de.stm.android.wowcharacter.R;
+
+/**
+ * Renderer fuer Zeilen des ItemsListViews
+ * 
+ * @author <a href="mailto:thomasfunke71@googlemail.com">Thomas Funke</a>, <a
+ *         href="mailto:stefan.moldenhauer@googlemail.com">Stefan Moldenhauer</a>
+ */
+@SuppressWarnings("unchecked")
+public class ItemListAdapter extends ArrayAdapter {
+	private static final int res = R.layout.characterviewtabitem;
+	private Activity context;
+	private ArrayList<Object[]> item;
+
+	/**
+	 * @param context
+	 * @param item
+	 */
+	public ItemListAdapter( Activity context, ArrayList<Object[]> item ) {
+		super( context, res, item );
+		this.context = context;
+		this.item = item;
+	}
+
+	@Override
+	public View getView( int position, View convertView, ViewGroup parent ) {
+		View row = convertView;
+		if (row == null) {
+			LayoutInflater inflater = context.getLayoutInflater();
+			row = inflater.inflate( res, null );
+		}
+		Object[] itemValues = item.get( position );
+		ImageView itemImage = (ImageView)row.findViewById( R.id.ItemImage );
+		Object o = itemValues[0];
+		if(o instanceof Bitmap) {
+			itemImage.setImageBitmap(  (Bitmap)o );
+		}
+		TextView name = (TextView)row.findViewById( R.id.ItemName );
+		name.setText( "name: " + itemValues[1].toString() );
+		return row;
+	}
+}
